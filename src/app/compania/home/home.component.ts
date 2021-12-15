f6438ce6dd157a33527abcc17f1b13237c4db332
Omar Hernandez <ing_omarhdez@hotmail.com>
2021-12-15 02:49:07 -0600

    Se agrega funcionalidad de home

--- src/app/compania/home/home.component.ts
+++ src/app/compania/home/home.component.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public optionSelectedCountry : string='NA';
+	public optionSelectedTitle : string='NA';
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  search(){
+    window.open('http://localhost:8095/'+this.optionSelectedCountry+'/'+this.optionSelectedTitle, '_blank');
+  }
 }
